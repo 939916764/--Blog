35dda69672b03092fb6db6401ccd4e71e81c373a
939916764 <939916764@qq.com>
2022-06-30 19:14:13 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import headNav from "./components/headNav.vue"; //头部导航
+</script>
+
+<template>
+  <div id="id">
+    <headNav></headNav>
+  </div>
+</template>
+
+<style>
+@import "./assets/css/base.css";
+</style>
---
+++ src/components/headNav.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+</script>
+<template>
+  <header>
+    <div class="mainbox">
+      <div class="logo">再度重相逢</div>
+      <div class="nav">
+        <ul>
+          <li>个人主页</li>
+          <li>项目记录</li>
+          <li>问题记录</li>
+        </ul>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="less" scoped>
+header {
+  position: fixed;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  box-shadow: 0 2px 4px 0 rgb(0 0 0 / 5%);
+  z-index: 222;
+  height: 55px;
+  .mainbox {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    ul{
+        display: flex;
+        li{
+            padding: 0 20px;
+            margin-right: 20px;
+            &:last-child{
+                margin-right: 0;
+            }
+        }
+    }
+  }
+}
+</style>
